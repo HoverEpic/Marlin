b28b05365adb0c7f022c3716ff384198ee570ebf
Epic <pcsrvadm@gmail.com>
2020-09-20 20:23:08 +0200

    change fan pin, fan is 5v => no mosfet

--- Marlin/src/pins/ramps/pins_MKS_GEN_L.h
+++ Marlin/src/pins/ramps/pins_MKS_GEN_L.h
@@ -37,6 +37,8 @@
 // Power outputs EFBF or EFBE
 #define MOSFET_D_PIN 7
 
+#define FAN_PIN         4
+
 //
 // CS Pins wired to avoid conflict with the LCD
 // See https://www.thingiverse.com/asset:66604
